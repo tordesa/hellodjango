e251ad6c07be52d32006e38d8ddba9e4f92f87d5
ajohnblessing <ajohnblessing@gmail.com>
2022-06-25 15:50:49 +0100

    added a view method

---
+++ homepage/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from homepage.views import HomepageView
+
+
+urlpatterns = [
+    path('', HomepageView.as_view(), name='home'),
+
+]
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
 # Create your views here.
+
+
+class HomepageView(TemplateView):
+    template_name = "index.html"
+
+    def good_bye(self):
+        return 'Goodbye'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_statement'] = 'Nice to see you!'
+        return context
+
+    
